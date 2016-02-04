dd687eb2e2a28c8eb222a095ad212c7c82b26381
mattskent <mattskent@gmail.com>
2016-02-04 14:03:56 -0500

    bot updates

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = cool() + "Hello World!";
 
   options = {
     hostname: 'api.groupme.com',
